67ad59f3b9575a073a5eb42e5ebed9524b8d43ec
kuzinalex <lyoxakuzin0@gmail.com>
2021-03-29 22:55:13 +0300

    added 2 classes for first task

---
+++ console-experiments/src/com/kuzin/Car.java
@@ -0,0 +1,13 @@
+package com.kuzin;
+
+public class Car{
+	private String name;
+
+	Car(String name){
+		this.name=name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+}
---
+++ console-experiments/src/com/kuzin/Main.java
@@ -0,0 +1,9 @@
+package com.kuzin;
+
+public class Main{
+
+	public static void main(String[] args){
+		Car myCar=new Car("Toyota RAV4");
+		System.out.println(myCar.getName()); 
+	}
+}
